2bfa57cb298349c195d8c2089e416094fb3308de
Ramses <machadoramses@yahoo.com>
2019-02-03 19:22:08 -0500

    progress01

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ramsesmachado/Desktop/FIU/Class2019/Product/CEN4010/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
