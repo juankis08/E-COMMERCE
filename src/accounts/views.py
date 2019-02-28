2189bc3aad000dc4088c1a5055862f3c933f8601
Ramses <machadoramses@yahoo.com>
2019-02-28 11:48:25 -0500

    Revert ""
    This reverts commit 1ad171c49b7ce796d353cbab7af89a5913b0925d.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -8,8 +8,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save() 
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'The account for {username} has been created! You are able to log in')
+            form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are able now to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
