6db7277d581e3c3568a9a7ba00c405ab0b1e192a
daykelms <daykelms@gmail.com>
2019-03-01 22:03:13 -0500

    Mayrelis update

--- src/bookstore/urls.py
+++ src/bookstore/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home_page'),
     path('about/', views.about, name='home_about'),
-    path('wishlist/', views.wishlist, name='wish_list')
 ]
