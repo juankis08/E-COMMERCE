d31fbd9bac28c0b6d36ca1c9673f8f2a59e040a7
daykelms <daykelms@gmail.com>
2019-04-04 20:24:51 -0400

    Revert "Address forms added"
    This reverts commit 7bba61ffa1e08ee9f9ddd2f5b34b2028f86feacf.

--- src/addresses/forms.py
+++ src/addresses/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 from .models import Address
 
-class AddressRegisterForm(forms.ModelForm):
+class AdressForm(forms.ModelForm):
     #class meta renders the the form as exactly is displayed in admin django web page
-    
     class Meta:
         model = Address
-        fields = ['address_type', 'address_line_1', 'address_line_2', 'city', 'state','zip_code', 'country']
--- src/addresses/views.py
+++ src/addresses/views.py
@@ -1,18 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import AddressRegisterForm
+from django.shortcuts import render
 
 # Create your views here.
-
-
-def addressregister(request):
-    if request.method == 'POST':
-        form = AddressRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(
-                request, f'Your account has been created! You are able now to log in')
-            return redirect('profile')
-    else:
-        form = AddressRegisterForm()
-    return render(request, 'addresses/address_form.html', {'form': form})
