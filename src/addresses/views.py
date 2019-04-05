2a0138f600f09a3004162260b40d4250cc7cba9b
daykelms <daykelms@gmail.com>
2019-04-04 21:19:57 -0400

    Address forms added
    Still does not register in the db

--- src/addresses/views.py
+++ src/addresses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import AddressRegisterForm
 
 # Create your views here.
+
+
+def addressregister(request):
+    if request.method == 'POST':
+        form = AddressRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f'Your address has been created! ')
+            return redirect('profile')
+    else:
+        form = AddressRegisterForm()
+    return render(request, 'addresses/address_form.html', {'form': form})
